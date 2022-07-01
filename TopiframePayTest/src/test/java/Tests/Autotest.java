b9ae239f42aec3317ac4ad92290cdd38918fbc3c
jerom55 <106803886+jerom55@users.noreply.github.com>
2022-07-01 19:31:40 +0300

    refactor code

--- TopiframePayTest/src/test/java/Tests/Autotest.java
+++ TopiframePayTest/src/test/java/Tests/Autotest.java
@@ -1,6 +1,5 @@
 package Tests;
 
-import org.junit.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
@@ -25,34 +24,29 @@ public class Autotest {
     @Test
     public void C65990() throws InterruptedException {
         String window = wd.getWindowHandle();
-        wd.get("https://topiframe.nsc-tech.ru/init-payment/9463");
-        Thread.sleep(5000);
-        // Вводим номер телефона который пополняем
-        WebElement phone = wd.findElement(By.xpath("//input[@type='tel']"));
-        phone.sendKeys("9032582114");
-        Thread.sleep(1000);
-        // Выбираем метод оплаты БК
-        WebElement bk = wd.findElement(By.xpath("//*[text()='С банковской карты']"));
-        bk.click();
+        phoneNumber("9032582114");
+        chanceSource();
+        paymentData("2200000000000004", "1224", "SERGEI IVANOV", "123" );
+        amountPay("500");
+        checkBox();
+        String text = capchaCode();
+        wd.switchTo().window(window);
+        inputCapch(text);
+        // оплата
+        WebElement pay = wd.findElement(By.xpath("//button[@type='submit']"));
+        pay.click();
+        Thread.sleep(10000);
+    }
+
+    private void inputCapch(String text) throws InterruptedException {
+        // Вставляем код капчи
+        Thread.sleep(2000);
+        WebElement capcha = wd.findElement(By.xpath("//input[@name='captcha']"));
+        capcha.sendKeys(text);
         Thread.sleep(3000);
-        // Вводим данные карты
-        WebElement pan = wd.findElement(By.xpath("//input[@name='Pan']"));
-        pan.sendKeys("2200000000000004");
-        WebElement exp = wd.findElement(By.xpath("//input[@name='ExpDate']"));
-        exp.sendKeys("1224");
-        WebElement name = wd.findElement(By.xpath("//input[@name='CardHolder']"));
-        name.sendKeys("SEGREI IVANOV");
-        WebElement cvv = wd.findElement(By.xpath("//input[@name='CVC']"));
-        cvv.sendKeys("123");
-        //Вводим сумму платежа
-        WebElement amount = wd.findElement(By.xpath("//input[@name='amount']"));
-        amount.sendKeys("500");
-        Thread.sleep(2000);
-        // Check-box
-        WebElement box = wd.findElement(By.xpath("//input[@type='checkbox']"));
-        Actions actions = new Actions(wd);
-        actions.moveToElement(box).clickAndHold().release().build().perform();
-        Thread.sleep(2000);
+    }
+
+    private String capchaCode() throws InterruptedException {
         // Код капчи
         WebElement pich = wd.findElement(By.cssSelector(".Service_captcha-img__2xct9"));
         String par = pich.getAttribute("currentSrc");
@@ -63,16 +57,50 @@ public class Autotest {
         String text = code.getAttribute("innerText");
         System.out.println(text);
         wd.close();
-        wd.switchTo().window(window);
-        // Вставляем код капчи
+        return text;
+    }
+
+    private void checkBox() throws InterruptedException {
+        // Check-box
+        WebElement box = wd.findElement(By.xpath("//input[@type='checkbox']"));
+        Actions actions = new Actions(wd);
+        actions.moveToElement(box).clickAndHold().release().build().perform();
         Thread.sleep(2000);
-        WebElement capcha = wd.findElement(By.xpath("//input[@name='captcha']"));
-        capcha.sendKeys(text);
+    }
+
+    private void amountPay(String summ) throws InterruptedException {
+        //Вводим сумму платежа
+        WebElement amount = wd.findElement(By.xpath("//input[@name='amount']"));
+        amount.sendKeys(summ);
+        Thread.sleep(2000);
+    }
+
+    private void paymentData(String pan, String exp, String holder, String cvv) {
+        // Вводим данные карты
+        WebElement number = wd.findElement(By.xpath("//input[@name='Pan']"));
+        number.sendKeys(pan);
+        WebElement date = wd.findElement(By.xpath("//input[@name='ExpDate']"));
+        date.sendKeys(exp);
+        WebElement name = wd.findElement(By.xpath("//input[@name='CardHolder']"));
+        name.sendKeys(holder);
+        WebElement code = wd.findElement(By.xpath("//input[@name='CVC']"));
+        code.sendKeys(cvv);
+    }
+
+    private void chanceSource() throws InterruptedException {
+        // Выбираем метод оплаты БК
+        WebElement bk = wd.findElement(By.xpath("//*[text()='С банковской карты']"));
+        bk.click();
         Thread.sleep(3000);
-        // оплата
-        WebElement pay = wd.findElement(By.xpath("//button[@type='submit']"));
-        pay.click();
-        Thread.sleep(10000);
+    }
+
+    private void phoneNumber(String targetPhone) throws InterruptedException {
+        wd.get("https://topiframe.nsc-tech.ru/init-payment/9463");
+        Thread.sleep(5000);
+        // Вводим номер телефона который пополняем
+        WebElement phone = wd.findElement(By.xpath("//input[@type='tel']"));
+        phone.sendKeys(targetPhone);
+        Thread.sleep(1000);
     }
 
     @AfterMethod
