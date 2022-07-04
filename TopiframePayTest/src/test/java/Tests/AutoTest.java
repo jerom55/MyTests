48eb76bbcfa07e7c675921175062e78a2e5de9f5
jerom55 <106803886+jerom55@users.noreply.github.com>
2022-07-04 18:35:49 +0300

    Добавил второй тест

--- TopiframePayTest/src/test/java/Tests/AutoTest.java
+++ TopiframePayTest/src/test/java/Tests/AutoTest.java
@@ -7,7 +7,7 @@ public class AutoTest extends TestBase {
     @Test
     public void C65990() throws InterruptedException {
         //Оплата услуги с БК без 3ds
-        getGoodUrl("https://topiframe.nsc-tech.ru/init-payment/9463");
+        getGoodsUrl("https://topiframe.nsc-tech.ru/init-payment/9463");
         String window = wd.getWindowHandle();
         fillPhoneNumber("9032582114");
         chanceSourcePayment();
@@ -18,7 +18,30 @@ public class AutoTest extends TestBase {
         wd.switchTo().window(window);
         inputCapchCode(text);
         clickPayButton();
+        waitingEndPayment();
     }
 
+    @Test
+    public void C65991() throws InterruptedException {
+        //Оплата услуги с БК c 3dsV1
+        getGoodsUrl("https://topiframe.nsc-tech.ru/init-payment/9463");
+        String window = wd.getWindowHandle();
+        fillPhoneNumber("9032582115");
+        chanceSourcePayment();
+        fillPaymentDetails("639002000000000003", "1224", "SERGEI IVANOV", "123");
+        setAmountPay("555");
+        tickedCheckBox();
+        String text = getCapchaCode();
+        wd.switchTo().window(window);
+        inputCapchCode(text);
+        clickPayButton();
+        confirmationPay();
+        waitingEndPayment();
+    }
+    @Test
+    public void C65992()throws InterruptedException {
+        getGoodsUrl("https://topiframe.nsc-tech.ru/init-payment/9463");
+        
+    }
 }
 
--- TopiframePayTest/src/test/java/Tests/TestBase.java
+++ TopiframePayTest/src/test/java/Tests/TestBase.java
@@ -1,32 +1,42 @@
 package Tests;
 
+import Application.ApplicationMeneger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class TestBase {
-    protected WebDriver wd;
+import java.time.Duration;
+
+public class TestBase extends ApplicationMeneger {
+    public WebDriver wd;
 
     @BeforeMethod
-    public void setUp()  {
-        System.setProperty("webdriver.chrome.driver", "F:\\AutoTest\\chrome\\chromedriver.exe");
-        wd = new ChromeDriver();
+    public void setUp() {
+        startTest();
 
     }
 
-    protected void clickPayButton() throws InterruptedException {
+    public void startTest() {
+        System.setProperty("webdriver.chrome.driver", "F:\\AutoTest\\chrome\\chromedriver.exe");
+        wd = new ChromeDriver();
+    }
+
+    public void clickPayButton() throws InterruptedException {
         // Жмём кнопу оплаты
         WebElement pay = wd.findElement(By.xpath("//button[@type='submit']"));
         pay.click();
         Thread.sleep(10000);
     }
 
-    protected void inputCapchCode(String text) throws InterruptedException {
+    public void inputCapchCode(String text) throws InterruptedException {
         // Вставляем код капчи полученный ранее
         Thread.sleep(2000);
         WebElement capcha = wd.findElement(By.xpath("//input[@name='captcha']"));
@@ -34,7 +44,7 @@ public class TestBase {
         Thread.sleep(3000);
     }
 
-    protected String getCapchaCode() throws InterruptedException {
+    public String getCapchaCode() throws InterruptedException {
         // Получаем код капчи
         WebElement pich = wd.findElement(By.cssSelector(".Service_captcha-img__2xct9"));
         String par = pich.getAttribute("currentSrc");
@@ -48,7 +58,7 @@ public class TestBase {
         return text;
     }
 
-    protected void tickedCheckBox() throws InterruptedException {
+    public void tickedCheckBox() throws InterruptedException {
         // Check-box ставим галочку
         WebElement box = wd.findElement(By.xpath("//input[@type='checkbox']"));
         Actions actions = new Actions(wd);
@@ -56,14 +66,14 @@ public class TestBase {
         Thread.sleep(2000);
     }
 
-    protected void setAmountPay(String summ) throws InterruptedException {
+    public void setAmountPay(String summ) throws InterruptedException {
         //Вводим сумму платежа
         WebElement amount = wd.findElement(By.xpath("//input[@name='amount']"));
         amount.sendKeys(summ);
         Thread.sleep(2000);
     }
 
-    protected void fillPaymentDetails(String pan, String exp, String holder, String cvv) {
+    public void fillPaymentDetails(String pan, String exp, String holder, String cvv) {
         // Вводим ДДК
         WebElement number = wd.findElement(By.xpath("//input[@name='Pan']"));
         number.sendKeys(pan);
@@ -75,26 +85,47 @@ public class TestBase {
         code.sendKeys(cvv);
     }
 
-    protected void chanceSourcePayment() throws InterruptedException {
+    public void chanceSourcePayment() throws InterruptedException {
         // Выбираем метод оплаты БК
         WebElement bk = wd.findElement(By.xpath("//*[text()='С банковской карты']"));
         bk.click();
         Thread.sleep(3000);
     }
 
-    protected void fillPhoneNumber(String targetPhone) throws InterruptedException {
+    public void fillPhoneNumber(String targetPhone) throws InterruptedException {
         // Вводим номер телефона который пополняем
         WebElement phone = wd.findElement(By.xpath("//input[@type='tel']"));
         phone.sendKeys(targetPhone);
         Thread.sleep(1000);
     }
 
-    protected void getGoodUrl(String url) throws InterruptedException {
+    public void getGoodsUrl(String url) throws InterruptedException {
         wd.get(url);
         Thread.sleep(5000);
     }
 
     @AfterMethod
-    public void tearDown() { wd.quit();
+    public void tearDown() {
+        stopTest();
     }
+
+    public void stopTest() {
+        wd.quit();
+    }
+
+    public void confirmationPay() {
+
+        WebElement confirm = (new WebDriverWait(wd, Duration.ofSeconds(30))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='password']"))));
+        confirm.sendKeys("12345678");
+    }
+
+    public void waitingEndPayment() throws InterruptedException {
+        Thread.sleep(10000);
+        WebElement confirmButton = wd.findElement(By.xpath("//div[@class='StatusPage_baddger-text__3h_Ls']"));
+        String succes = confirmButton.getAttribute("innerText");
+        System.out.println(succes);
+        Assert.assertEquals(succes, "Оплата успешно проведена.", "Test Fail");
+    }
+
 }
