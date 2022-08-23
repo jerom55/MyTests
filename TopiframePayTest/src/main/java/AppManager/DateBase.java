3e3945614d8f1b96a4c31a0f30168e8b30c9e21d
jerom55 <106803886+jerom55@users.noreply.github.com>
2022-08-23 16:30:23 +0300

    1

---
+++ TopiframePayTest/src/main/java/AppManager/DateBase.java
@@ -0,0 +1,27 @@
+package AppManager;
+
+import org.testng.annotations.Test;
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
+
+import java.sql.*;
+
+public class DateBase {
+
+    @Test
+
+    public void testDbConnection() throws ClassNotFoundException {
+
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+        Connection conn = null;
+    try{
+        conn = DriverManager.getConnection("jdbc:sqlserver://192.168.20.237;databaseName=MAINDB;user=s.ivanov@ruru.ru;password=AB652@77dc52");
+
+        Statement stmt = conn.createStatement();
+    } catch (SQLException ex){
+        System.out.println("SQLException " + ex.getMessage());
+        System.out.println("SqLState " + ex.getSQLState());
+        System.out.println("VendorError " + ex.getErrorCode());
+         }
+    }
+}
---
+++ TopiframePayTest/src/main/java/Bot/JenkinsSerj_bot.java
@@ -0,0 +1,13 @@
+package Bot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSerj_bot {
+
+    public static void main (String[] args){
+        SpringApplication.run (JenkinsSerj_bot.class, args);
+    }
+}
